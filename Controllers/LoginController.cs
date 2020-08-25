55e84fc3161837681cf0d090ddcfd3529e3ca006
Gemma Fox <gemma.fox.468@gmail.com>
2020-08-25 16:01:29 +1200

    Created Login Controller Created Login View Get data from Login form

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentLabManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult form1(string txtuser, string pwpw)
+        {
+            ViewBag.Id = txtuser;
+            ViewBag.Name = pwpw;
+            Console.WriteLine(txtuser);
+           
+
+            return View("Index");
+        }
+
+    }
+
+
+}
